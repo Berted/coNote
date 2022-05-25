814ac9e4e19e47a77fb5392157c64ea9ab0d40ea
Berted <bertedberted@gmail.com>
2022-05-25 22:37:08 +0700

    Add basic home page

--- conote-frontend/src/App.tsx
+++ conote-frontend/src/App.tsx
@@ -3,12 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Input } from '@chakra-ui/react';
 import Editor from './components/editor/Editor';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>coNote</h1>
-      <Editor />
+      <Home />
     </div>
   );
 }
