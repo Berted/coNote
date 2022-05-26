1a4075ae3798f184876c3fc95f1f7e574e806935
Berted <bertedberted@gmail.com>
2022-05-26 17:44:55 +0700

    Modified title color

--- conote-frontend/src/components/Home.tsx
+++ conote-frontend/src/components/Home.tsx
@@ -6,7 +6,8 @@ import {
   HStack,
   Button,
   Container,
-  Link
+  Link,
+  chakra,
 } from "@chakra-ui/react";
 
 export default function Home() {
@@ -16,9 +17,15 @@ export default function Home() {
         <VStack spacing="3em">
           <VStack spacing="-5px" marginTop="35vh">
             <Heading size="4xl" fontSize="72pt" fontFamily="League Spartan">
-              coNote
+              <chakra.span color="blue.700">co</chakra.span>
+              <chakra.span color="blue.400">Note</chakra.span>
             </Heading>
-            <Text size="l" color="gray.500" fontSize="18pt" fontFamily="DM Sans">
+            <Text
+              size="l"
+              color="gray.500"
+              fontSize="18pt"
+              fontFamily="DM Sans"
+            >
               Simply type.
             </Text>
           </VStack>
@@ -26,9 +33,18 @@ export default function Home() {
             <Button colorScheme="blue" boxShadow="base" padding="0px 2em">
               Login
             </Button>
-            <Button boxShadow="base" padding="0px 2em"> Signup</Button>
+            <Button boxShadow="base" padding="0px 2em">
+              {" "}
+              Signup
+            </Button>
           </HStack>
-        <Link href="https://github.com/Berted/coNote" target="_blank" color="gray.400">What is coNote?</Link>
+          <Link
+            href="https://github.com/Berted/coNote"
+            target="_blank"
+            color="gray.400"
+          >
+            What is coNote?
+          </Link>
         </VStack>
       </Center>
     </Container>
