4455e6b77b2402ee75508e167c4a660cbfd266d5
Rama Aryasuta Pangestu <rama.a.pangestu@gmail.com>
2022-05-29 23:19:19 +0700

    Add auto-redirect for page

--- conote-frontend/src/App.tsx
+++ conote-frontend/src/App.tsx
@@ -7,17 +7,44 @@ import Home from "./components/Home";
 import Login from "./components/user/Login";
 import Signup from "./components/user/Signup";
 import Dashboard from "./components/user/Dashboard";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, Navigate } from "react-router-dom";
+import { useProvideAuth } from "hooks/useAuth";
 
 function App() {
+  const authentication = useProvideAuth();
+
+  const RedirectRoute = (
+    { isAllowed, redirectPath, children }:
+      { isAllowed: any, redirectPath: string, children: any }
+  ) => {
+    if (!isAllowed) {
+      return <Navigate to={redirectPath} replace />;
+    } else {
+      return children ? children : <Outlet />;
+    }
+  };
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="signup" element={<Signup />}></Route>
-          <Route path="dashboard" element={<Dashboard />}></Route>          
+          <Route element={<RedirectRoute
+            isAllowed={!authentication.user}
+            redirectPath="dashboard"
+            children={undefined}
+          />}>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="signup" element={<Signup />} />
+          </Route>
+          <Route path="dashboard" element={
+            <RedirectRoute
+              isAllowed={!!authentication.user}
+              redirectPath="/"
+            >
+              <Dashboard />
+            </RedirectRoute>
+          } />
         </Routes>
       </div>
     </BrowserRouter>
