52efc1b880b5ca6a9a8877b40db32a8ee27d0eb2
Rama Aryasuta Pangestu <rama.a.pangestu@gmail.com>
2022-06-21 01:56:41 +0700

    Add reset input after successful submission

--- conote-frontend/src/components/user/Forgetpassword.tsx
+++ conote-frontend/src/components/user/Forgetpassword.tsx
@@ -11,42 +11,10 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useProvideAuth } from "hooks/useAuth";
 
-function Forgetpasswordform(props: any) {
-  const [email, setEmail] = useState("");
-
-  return (
-    <VStack
-      boxShadow="base"
-      borderRadius="md"
-      padding="7"
-      spacing="5"
-      w="70vw"
-      minW="340px"
-      maxW="lg"
-    >
-      <FormControl id="email">
-        <FormLabel htmlFor="email">Email address</FormLabel>
-        <Input
-          onChange={(e) => setEmail(e.target.value)}
-          type="email"
-          placeholder="johndoe@example.com"
-        />
-      </FormControl>
-      <Button
-        onClick={() => props.onButtonClick(email)}
-        colorScheme="blue"
-        boxShadow="base"
-        padding="0px 1.5em"
-      >
-        {props.buttonValue}
-      </Button>
-    </VStack>
-  );
-}
-
 export default function Forgetpassword() {
   const navigate = useNavigate();
   const authentication = useProvideAuth();
+  const [email, setEmail] = useState("");
 
   return (
     <Flex minH={"100vh"} align={"center"} justify={"center"}>
@@ -54,13 +22,37 @@ export default function Forgetpassword() {
         <Heading size="3xl" fontFamily="League Spartan">
           Reset Password
         </Heading>
-        <Forgetpasswordform
-          onButtonClick={(email: string) => {
-            authentication.sendPwdResetEmail(email)
-              .catch(error => console.log(error));
-          }}
-          buttonValue={"Recover"}
-        />
+        <VStack
+          boxShadow="base"
+          borderRadius="md"
+          padding="7"
+          spacing="5"
+          w="70vw"
+          minW="340px"
+          maxW="lg"
+        >
+          <FormControl id="email">
+            <FormLabel htmlFor="email">Email address</FormLabel>
+            <Input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="johndoe@example.com"
+            />
+          </FormControl>
+          <Button
+            onClick={() => {
+              authentication.sendPwdResetEmail(email)
+                .then(response => setEmail(""))
+                .catch(error => console.log(error));
+            }}
+            colorScheme="blue"
+            boxShadow="base"
+            padding="0px 1.5em"
+          >
+            Recover
+          </Button>
+        </VStack>
       </VStack>
     </Flex>
   );
