01b5005771429caeec0c7afee6ae8c12193e2534
Berted <bertedberted@gmail.com>
2022-06-21 15:21:28 +0700

    File restructuring - minor fix

---
+++ conote-frontend/src/components/user/UserButton.tsx
@@ -0,0 +1,55 @@
+import {
+  Avatar,
+  Button,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverArrow,
+} from "@chakra-ui/react";
+
+import { useNavigate } from "react-router-dom";
+import { useProvideAuth } from "hooks/useAuth";
+
+export default function UserButton(props: any) {
+  const navigate = useNavigate();
+  const auth = useProvideAuth();
+
+  return (
+    <Popover placement="bottom-end" autoFocus={false}>
+      <PopoverTrigger>
+        <Avatar
+          bg="blue.400"
+          transition="background-color 100ms linear"
+          _hover={{
+            bg: "blue.600",
+          }}
+          role="button"
+        />
+      </PopoverTrigger>
+      <PopoverContent boxShadow="sm">
+        <PopoverArrow />
+        <PopoverHeader>
+          Hi, <b>{auth.userData === undefined ? "" : auth.userData.fullname}</b>
+          !
+        </PopoverHeader>
+        <PopoverBody>
+          <Button
+            onClick={() => {
+              auth
+                .signout()
+                .then((response: any) => navigate("/"))
+                .catch((error: any) => console.log(error));
+            }}
+            colorScheme="blue"
+            boxShadow="base"
+            padding="0px 1.5em"
+          >
+            Logout
+          </Button>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  );
+}
