0fa740ffebeac712bc325d6545651b8f9eacef59
Berted <bertedberted@gmail.com>
2022-06-23 18:07:52 +0700

    Adds UML support

--- conote-frontend/src/components/editor/Editor.tsx
+++ conote-frontend/src/components/editor/Editor.tsx
@@ -11,6 +11,7 @@ import ReactMarkdown from "react-markdown";
 import "./github-markdown-light.css";
 import remarkMath from "remark-math";
 import remarkGfm from "remark-gfm";
+import remarkSimpleUML from "@akebifiky/remark-simple-plantuml";
 import rehypeHighlight from "rehype-highlight";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
@@ -31,7 +32,7 @@ function MarkdownPreview({ docContent, ...props }: any) {
       <ReactMarkdown
         children={docContent}
         className="markdown-body"
-        remarkPlugins={[remarkMath, remarkGfm]}
+        remarkPlugins={[remarkMath, remarkGfm, remarkSimpleUML]}
         rehypePlugins={[rehypeKatex]}
       />
     </>
